a04f8084c1d3252dcf097a30da725317c74b8af8
Jillo Wario <willywario0@gmail.com>
2026-02-07 23:58:29 +0200

    cors policy update for backend

--- creator-dashboard/src/pages/_app.js
+++ creator-dashboard/src/pages/_app.js
@@ -1,10 +1,9 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
 import store from "@/store/store";
 import { Provider } from "react-redux";
 import { Toaster } from "sonner";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <Toaster position="bottom-right" />
