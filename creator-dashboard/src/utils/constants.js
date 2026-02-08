fc8b09991da1a61763cfcce01712273be549ed4c
Jillo Wario <willywario0@gmail.com>
2026-02-08 18:14:31 +0200

    frontend production vars

--- creator-dashboard/src/utils/constants.js
+++ creator-dashboard/src/utils/constants.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = "https://creator-dashboard-production-c49b.up.railway.app";
-export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "https://creator-dashboard-production-c49b.up.railway.app";
+// export const BASE_URL = "http://localhost:5000";
