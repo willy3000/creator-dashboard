afd09e0c5b43f98459cab858b08b801bfd6ae839
Jillo Wario <willywario0@gmail.com>
2026-02-08 18:19:27 +0200

    cors policy update

--- creator-dashboard-backend/index.js
+++ creator-dashboard-backend/index.js
@@ -1,43 +1,3 @@
-// const express = require("express");
-// const jwt = require("jsonwebtoken");
-// const bcrypt = require("bcryptjs");
-// const dotenv = require("dotenv");
-// const cookieParser = require("cookie-parser");
-// const http = require("http");
-// const { Server } = require("socket.io");
-
-// const app = express();
-
-// dotenv.config();
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// var cors = require("cors");
-
-// const corsOptions = {
-//   // origin: ["http://localhost:3000", "http://192.168.100.69:3000"],
-//   origin: [
-//     "http://localhost:3000",
-//     "http://192.168.100.6:3000",
-//     "https://creator-dashboard-puce.vercel.app",
-//   ],
-//   methods: ["GET", "POST", "PUT", "DELETE"],
-// };
-
-// // app.use(cors());
-// app.use(cors(corsOptions));
-// app.use(cookieParser()); // Make sure this is used before your routes
-
-// //Authentication
-// app.use("/api/auth", require("./routes/api/auth"));
-// app.use("/api/assets", require("./routes/api/assets"));
-
-// const PORT = process.env.PORT || 5000;
-
-// app.listen(PORT, () => console.log(`Server started  on port ${PORT}`));
-
-
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
@@ -45,43 +5,87 @@ const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const http = require("http");
 const { Server } = require("socket.io");
-const cors = require("cors");
-
-dotenv.config();
 
 const app = express();
 
+dotenv.config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+var cors = require("cors");
+
 const corsOptions = {
+  // origin: ["http://localhost:3000", "http://192.168.100.69:3000"],
   origin: [
     "http://localhost:3000",
     "http://192.168.100.6:3000",
     "https://creator-dashboard-puce.vercel.app",
   ],
-  credentials: true, 
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: [
-    "Content-Type",
-    "Authorization",
-    "X-Requested-With",
-  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
 };
 
+// app.use(cors());
 app.use(cors(corsOptions));
+app.use(cookieParser()); // Make sure this is used before your routes
 
-app.options("*", cors(corsOptions));
-
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(cookieParser());
-
-
+//Authentication
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/assets", require("./routes/api/assets"));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server started  on port ${PORT}`));
+
+
+
+
+
+
+// const express = require("express");
+// const jwt = require("jsonwebtoken");
+// const bcrypt = require("bcryptjs");
+// const dotenv = require("dotenv");
+// const cookieParser = require("cookie-parser");
+// const http = require("http");
+// const { Server } = require("socket.io");
+// const cors = require("cors");
+
+// dotenv.config();
+
+// const app = express();
+
+// const corsOptions = {
+//   origin: [
+//     "http://localhost:3000",
+//     "http://192.168.100.6:3000",
+//     "https://creator-dashboard-puce.vercel.app",
+//   ],
+//   credentials: true, 
+//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//   allowedHeaders: [
+//     "Content-Type",
+//     "Authorization",
+//     "X-Requested-With",
+//   ],
+// };
+
+// app.use(cors(corsOptions));
+
+// app.options("*", cors(corsOptions));
+
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// app.use(cookieParser());
+
+
+// app.use("/api/auth", require("./routes/api/auth"));
+// app.use("/api/assets", require("./routes/api/assets"));
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server started on port ${PORT}`);
+// });
